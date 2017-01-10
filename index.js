1f6585d6a76bbcf9c3650c228fed56ed709762d6
Rodrigo Adones Vicencio <RodigoAdones@MacBook-Pro-de-Rodrigo.local>
2017-01-10 15:35:26 -0300

    connect and disconnect user message

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ app.get('/', function (req, res) {
 })
 
 io.on('connection', function (socket) {
+  io.emit('chat message', 'A user connect')
   socket.on('chat message', function (msg) {
     io.emit('chat message', msg)
+  })
+  socket.on('disconnect', function () {
+    io.emit('chat message', 'A user left the room')
   })
 })
 
